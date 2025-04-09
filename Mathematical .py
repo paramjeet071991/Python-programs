8356a07a29363303d68f188a521cfd782f31da17
paramjeet071991 <103423854+paramjeet071991@users.noreply.github.com>
2025-04-09 23:32:01 +0530

    Create Mathematical .py

---
+++ Mathematical .py
@@ -0,0 +1,45 @@
+a=int(input('a: '))
+      b=int(input('b: '))
+c=(a*b)
+print('The result of the Multiplication is :',c)
+a=int(input('a: '))
+b=int(input('b: '))
+c=(a+b)
+print('The Result of the Addition is: ',c)
+a=int(input('a: '))
+b=int(input('b: '))
+c=(a-b)
+print('The Result of the Substraction is: ',c)
+a=int(input('a: '))
+b=int(input('b: '))
+c=(a/b)
+print('The Result of the Divide is: ',c)
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+9
